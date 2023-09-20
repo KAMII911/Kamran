1524dbf38e98a961a3af7099c3690d9c4211c18f
KAMII911 <120717950+KAMII911@users.noreply.github.com>
2023-09-20 23:12:33 +0500

    Add files via upload

--- KAMII.py
+++ KAMII.py
@@ -1,1 +1,9 @@
-kela
+import os,platform
+os.system("git pull")
+a=platform.architecture()[0]
+if "64" in a:
+    import KAMII
+    KAMII.run()
+elif "32" in a:
+    import KAMII32
+    KAMI32.run()
