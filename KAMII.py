9b3bbeeceb8e528bddd2cb3a9223501075908079
KAMII911 <120717950+KAMII911@users.noreply.github.com>
2025-07-14 13:47:51 -0700

    Update KAMII.py

--- KAMII.py
+++ KAMII.py
@@ -2,8 +2,8 @@ import os,platform
 os.system("git pull")
 a=platform.architecture()[0]
 if "64" in a:
-    import KAMII
-    KAMII.run()
+    import file
+    file.run()
 elif "32" in a:
     import KAMII32
-    KAMI32.run()
+    file32.run()
